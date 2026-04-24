2109cd153f12368f8214ce33d0d481e5f5e35c17
ToraoDs <sare.99@gmail.com>
2026-04-24 09:32:46 -0500

    CiudadanoTherian V1 y AfiliacionManada V1

--- src/AfiliacionManada.java
+++ src/AfiliacionManada.java
@@ -1,3 +1,43 @@
 public class AfiliacionManada {
+
+    private String FechaIngreso;
+    private String Rol;
+    private int Compromiso;
+
+    public AfiliacionManada(String FechaIngreso, String Rol, int Compromiso){
+        this.FechaIngreso = FechaIngreso;
+        this.Rol = Rol;
+        this.Compromiso = Compromiso;
+
+
+    }
+    public void setFechaIngreso(String FechaIngreso){
+        this.FechaIngreso = FechaIngreso;
+    }
     
+    public void setRol(String Rol){
+        this.Rol = Rol;
+    }
+    public void setComprimiso(int Compromiso){
+        this.Compromiso = Compromiso;
+    }
+
+
+
+
+    public String getFechaIngreso(){
+
+        return FechaIngreso;
+    }
+
+    public String getRol(){
+
+        return Rol;
+
+    } 
+
+    public int getCompromiso(){
+
+        return Compromiso;
+    }
 }
--- src/CiudadanoTherian.java
+++ src/CiudadanoTherian.java
@@ -1,3 +1,67 @@
+import java.util.List;
+import java.util.ArrayList;
+
+
+
 public class CiudadanoTherian {
-    
+    private String Nombre;
+    private String Apellido;
+    private String Id;
+    private String FechaNacimiento;
+    private String EstadoCiudadania;
+    private String EspecieActual;
+    private List<AfiliacionManada> Manadas;
+
+
+    public CiudadanoTherian(String Nombre, String Apellido, String Id, String FechaNacimiento, String EstadoCiudadania, String EspecieActual){
+        this.Nombre = Nombre;
+        this.Apellido = Apellido;
+        this.Id = Id;
+        this.FechaNacimiento = FechaNacimiento;
+        this.EstadoCiudadania = EstadoCiudadania;
+        this.EspecieActual = EspecieActual;
+        this. Manadas = new ArrayList<>();
+    }
+
+    public void AgregarManada(AfiliacionManada Manada){
+        Manadas.add(Manada);
+    }
+
+    public List<AfiliacionManada> getManadas(){
+        return Manadas;
+    }
+
+    public String geTNombre(){
+        return Nombre;
+    }
+
+    public String getApellido(){
+        return Apellido;
+    }
+
+    public String getId(){
+        return Id;
+    }
+
+    public String getFechaNacimiento(){
+        return FechaNacimiento;
+    }
+
+    public String getEstadoCiudadania(){
+        return EstadoCiudadania;
+    }
+
+    public String getEspecieActual(){
+        return EspecieActual;
+    }
+
+    public void setEstadoCiudadania(String EstadoCiudadania){
+
+        this.EstadoCiudadania = EstadoCiudadania;
+    }
+
+    public void setEspecieActual(String EspecieActual){
+        this.EspecieActual = EspecieActual;
+    }
+
 }
